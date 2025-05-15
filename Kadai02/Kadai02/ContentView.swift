a0f1996fc409c4c2d9ad4928347cb134a97e09ec
ios1a <ios1a@iMac26.local>
2025-05-15 10:41:19 +0900

    aa

---
+++ Kadai02/Kadai02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kadai02
+//
+//  Created by ios1a on 2025/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kadai02/Kadai02/Kadai02App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai02App.swift
+//  Kadai02
+//
+//  Created by ios1a on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct Kadai02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
