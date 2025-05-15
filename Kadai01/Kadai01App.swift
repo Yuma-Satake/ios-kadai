71f7a98c45abe885c370c19e631670de3eea9ff2
ios1a <ios1a@iMac26.local>
2025-05-15 10:07:21 +0900

    init

---
+++ Kadai01/Kadai01App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai01App.swift
+//  Kadai01
+//
+//  Created by ios1a on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct Kadai01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kadai01Tests/Kadai01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai01Tests.swift
+//  Kadai01Tests
+//
+//  Created by ios1a on 2025/05/15.
+//
+
+import Testing
+@testable import Kadai01
+
+struct Kadai01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
